0d58f8dd3f3a8176d67f3fb777855d084eb80b57
Jonathan Torres <jontorres91@hotmail.com>
2026-05-16 17:49:34 -0700

    Created the main blueprint and registered it in the app factory function. Also imported the main routes to the init__.py, tested and passed.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from app.main import main
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(main)
     return app
---
+++ app/main/routes.py
@@ -0,0 +1,4 @@
+from app.main import main 
+@main.route('/') 
+def index():
+   return "LootAlert Home"
