c855bb0bbaf369bc50b4a3f481f49881df3e4733
Jonathan Torres <jontorres91@hotmail.com>
2026-05-17 22:25:00 -0700

    Added container div around content blocks in base. added watchlist page table and placeholder entry for watchlist routes removed container div from home page.

--- app/watchlist/routes.py
+++ app/watchlist/routes.py
@@ -2,4 +2,11 @@ from app.watchlist import watchlist
 from flask import render_template
 @watchlist.route('/watchlist') 
 def watchlist():
-   return render_template('watchlist/watchlist.html') 
+   games = [
+        {
+            "name": "Elden Ring",
+            "current_price": 39.99,
+            "desired_price": 25.00
+        }
+    ]
+   return render_template("watchlist/watchlist.html", games=games) 
